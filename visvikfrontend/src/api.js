ae9f6da929222fc28c911efcce00fdf26130ea36
visvikBusinessSolutions <yogesh@visvik.in>
2025-08-22 16:03:35 +0530

    Update api.js

--- visvikfrontend/src/api.js
+++ visvikfrontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL, // env se lega
+  baseURL: import.meta.env.VITE_API_URL, // ✅ vite env variable
   headers: {
     "Content-Type": "application/json",
   },
